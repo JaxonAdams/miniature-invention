35126c21e11c57905182c945b8125a97aa75f70b
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-01 20:40:21 -0600

    initial files

---
+++ client/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <h1 className='title'>Let's Celebrate!</h1>
+        </div>
+    );
+};
+
+export default Hero;
